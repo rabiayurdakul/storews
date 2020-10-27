1e52b47670bb8dfe2b6716892fdb2f551aa56c99
rabia.yurdakul <rabiayurdakul@gmail.com>
2020-10-27 16:48:12 +0300

    Added create-product page

--- src/main/java/com/eleia/storews/controller/ViewController.java
+++ src/main/java/com/eleia/storews/controller/ViewController.java
@@ -1,10 +1,16 @@
 package com.eleia.storews.controller;
 
+import com.eleia.storews.model.Product;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class ViewController {
+
+    @Autowired
+    ProductWs productWs;
 
     @RequestMapping("/view-products")
     public String viewProducts(){
@@ -14,5 +20,17 @@ public class ViewController {
     @RequestMapping("add-product")
     public String addProduct(){
         return "add-product";
+    }
+    @GetMapping("/create-product")
+    public String addProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "create-product";
+    }
+
+    @PostMapping("/create-product")
+    public String addProduct(@ModelAttribute Product product, Model model){
+        model.addAttribute("product", product);
+        productWs.createProduct(product);
+        return "view-products";
     }
 }
